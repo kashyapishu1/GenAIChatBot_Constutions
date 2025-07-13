b635a60fd06306b640e857a4764a2bf45b24c64f
Indu Bhushan <bhushan.i@avowstech.com>
2025-07-13 21:20:32 +0530

    implemented the main

--- ChatBot.py
+++ ChatBot.py
@@ -7,40 +7,50 @@ from langchain_community.vectorstores import FAISS
 
 OPENAI_API_KEY=""
 
-#Upload PDF files
-st.header("My first Chatbot")
+def inti():
+    #Upload PDF files
+    st.header("My first Chatbot")
 
 
-with  st.sidebar:
-    st.title("Your Documents")
-    file = st.file_uploader(" Upload a PDf file and start asking questions", type="pdf")
+    with  st.sidebar:
+        st.title("Your Documents")
+        file = st.file_uploader(" Upload a PDf file and start asking questions", type="pdf")
 
 
-#Extract the text
-if file is not None:
-    pdf_reader = PdfReader(file)
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-        #st.write(text)
+    #Extract the text
+    if file is not None:
+        pdf_reader = PdfReader(file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            #st.write(text)
 
 
-#Break it into chunks
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators="\n",
-        chunk_size=1000,
-        chunk_overlap=150,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    #st.write(chunks)
+    #Break it into chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators="\n",
+            chunk_size=1000,
+            chunk_overlap=150,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+        #st.write(chunks)
 
 
 
 
-    # generating embedding
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        # generating embedding
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 
-    # creating vector store - FAISS
-    vector_store = FAISS.from_texts(chunks, embeddings)
+        # creating vector store - FAISS
+        vector_store = FAISS.from_texts(chunks, embeddings)
+
+
+def main():
+    # Your main logic goes here
+    inti()
+
+if __name__ == "__main__":
+    main()
+
